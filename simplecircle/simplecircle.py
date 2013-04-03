4340b1ad4b7e8402e4eb28b4165b9f386f526ae0
Luis Artola <la@luisartola.com>
2013-04-03 05:39:25 -0700

    Fixed typo.

--- simplecircle/simplecircle.py
+++ simplecircle/simplecircle.py
@@ -25,7 +25,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 '''
-Demostrates how to use respond to mouse motion and mouse button down events
+Demonstrates how to use respond to mouse motion and mouse button down events
 sent wirelessly by Remote Pi application running on iPhone, iPad and iPod
 touch. Displays a green circle on a window that follows the mouse cursor
 as you drag your finger on your iOS device surface. Tap to pin a circle at
